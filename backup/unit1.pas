253036f6185db9fdfe8579a67fedbc3242522e19
GithubDroid2006 <75797971+GithubDroid2006@users.noreply.github.com>
2020-12-10 16:37:58 +0100

    Add files via upload

---
+++ backup/unit1.pas
@@ -0,0 +1,47 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  label1.Caption := 'Hallo, ' + Edit1.Text + '!'
+
+end;
+
+end.
+
